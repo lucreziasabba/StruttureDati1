7d5719bb08213b6371988a28ac480bcc91816cc7
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-27 09:16:01 +0100

    Add project files.

---
+++ File/FileApp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace File
+{
+    class FileApp
+    {
+        private const string file = "file.txt";
+        private const string file2 = "finito.txt";
+        static void Main(string[] args)
+        {
+            List<double> numeri = new List<double>();
+            using (StreamReader x = new StreamReader(file, Encoding.UTF8))
+            {
+                string sline;
+                while((sline = x.ReadLine()) != null)
+                {
+                    double y = Convert.ToDouble(sline);
+                    
+                    numeri.Add(y);
+                }
+            }
+            using (StreamWriter f = new StreamWriter(file2, true))
+            {
+                for (int i= numeri.Count-1; i>=0; i--)
+                {
+                    f.WriteLine(numeri[i]);
+                }
+            }
+
+        }
+    }
+        
+}
